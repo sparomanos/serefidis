d5884fa8f9af5df2ac7296656957c84189c51351
serefidis <manos@serefidis.gr>
2020-07-28 11:10:51 +0300

    initialize

---
+++ API/Models/Specialty.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Specialty
+    {
+        public string Description { get; set; }
+    }
+}
